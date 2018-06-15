e8e5dd9fa3fde5680f3457b558ddbc74489760b9
Alan Shum <alan200994@gmail.com>
2018-06-14 21:16:12 -0700

    do not need debug anymore

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const filesPath = {
   js: {
     src: path.resolve('./src/**/*.js'),
-    dist: path.resolve('./dist'),
-    debug_src: [path.resolve('./spec/**/*.js'), path.resolve('./src/**/*.js')],
-    debug_dist: path.resolve('./debug')
+    dist: path.resolve('./dist')
   }
 };
 
@@ -16,14 +14,6 @@ gulp.task('build:js', function() {
   return gulp.src(filesPath.js.src)
   .pipe(babel())
   .pipe(gulp.dest(filesPath.js.dist));
-});
-
-gulp.task('debug:js', function() {
-  return gulp.src(filesPath.js.debug_src, { base: '.' })
-  .pipe(sourcemaps.init())
-  .pipe(babel())
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest(filesPath.js.debug_dist));
 });
 
 gulp.task('watch', function() {
