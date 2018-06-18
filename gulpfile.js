f9ea31607ff46ebaf4e364ef102c25ed22a7017c
Alan Shum <alan200994@gmail.com>
2018-06-17 20:57:58 -0700

    added eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
 const path = require('path');
-const sourcemaps = require('gulp-sourcemaps');
+const eslint = require('gulp-eslint');
 
 const filesPath = {
   js: {
@@ -10,14 +10,31 @@ const filesPath = {
   }
 };
 
-gulp.task('build:js', function() {
-  return gulp.src(filesPath.js.src)
-  .pipe(babel())
-  .pipe(gulp.dest(filesPath.js.dist));
-});
+initLintTasks();
+initBuildTasks();
 
 gulp.task('watch', function() {
   gulp.watch(filesPath.js.src, gulp.series('build:js'));
 });
 
-gulp.task("default", gulp.series('build:js'));
+gulp.task('dist:js', gulp.series('lint:js', 'build:js'));
+gulp.task('dist', gulp.parallel('dist:js'));
+
+gulp.task("default", gulp.series('dist'));
+
+function initLintTasks() {
+  gulp.task('lint:js', function() {
+    return gulp.src(filesPath.js.src)
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
+  });
+}
+
+function initBuildTasks() {
+  gulp.task('build:js', function() {
+    return gulp.src(filesPath.js.src)
+      .pipe(babel())
+      .pipe(gulp.dest(filesPath.js.dist));
+  });
+}
