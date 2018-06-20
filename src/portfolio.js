37cd07b45625afbfe23d6fcb6809180dcec50eee
Alan Shum <alan200994@gmail.com>
2018-06-20 09:23:02 -0700

    recursive fx to get instrument symbols

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,4 +1,5 @@
-// import fetch from 'node-fetch';
+import fetch from 'node-fetch';
+import fs from 'fs';
 import Robinhood from './robinhood';
 
 export default class Portfolio extends Robinhood {
@@ -7,8 +8,34 @@ export default class Portfolio extends Robinhood {
   }
 
   async get() {
-    const orderHistory = await this.getDataFromAPI('next', ['instrument', 'quantity', 'average_price', 'side', 'last_transaction_at']);
-    // TODO: integrate instrument chain symbol
+    // const orderHistory = await this.getDataFromAPI('next', ['instrument', 'quantity', 'average_price', 'side', 'last_transaction_at']);
+    const orderHistory = await fs.readFile('test/robinhood.orderhistory.json');
+    this.mapInstrument(orderHistory);
+  }
+
+  async mapInstrument(orderHistory) {
+    const stocksURLs = Array.from(new Set(orderHistory.map(order => order.instrument)));
+    const batchSize = 10;
+    return this.batchRequest(stocksURLs, batchSize);
+  }
+
+  async batchRequest(urls, batchSize, batchIndex = 0, results = []) {
+    const batchPromise = urls.map(async (url) => {
+      const fetchResponse = await fetch(url, { methods: 'GET' });
+      const rawResponse = await fetchResponse.json();
+      return this.filteredHash(rawResponse, ['name', 'symbol', 'country']);
+    });
+
+    const batchResults = await Promise.all(batchPromise);
+    const appendedResults = [...results, ...batchResults];
+
+    const nextBatchIndex = batchIndex;
+    const nextBatchUrlSet = urls.splice(nextBatchIndex * batchSize, batchSize);
+    if (nextBatchUrlSet.length) {
+      return this.batchRequest(nextBatchUrlSet, batchSize, nextBatchIndex, appendedResults);
+    }
+
+    return results;
   }
 
   // async portfolioWithInstrumentsInfo(instruments) {
