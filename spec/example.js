69abe498558fc9be6a4102472e553c0f73ae0c2b
Alan Shum <alan200994@gmail.com>
2018-07-31 23:49:59 -0700

    updated readme

--- spec/example.js
+++ spec/example.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // testMode: request will be saved to .cached-fetch,
 // and further request to the same path will be served from those cached file
-// allowing reloading and testing interatively without API limit from Robinhood
+// allowing reloading and testing interatively with no API limit from Robinhood
 // const portfolio = new Portfolio(process.env.ROBINHOOD_TOKEN);
 const portfolio = new Portfolio(process.env.ROBINHOOD_TOKEN, { testMode: true });
 portfolio.getOrderHistory()
